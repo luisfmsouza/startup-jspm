f387ac703f360eaf7985144fc28a42991a0153e2
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-11-10 18:12:50 -0200

    Including bundle scripts

---
+++ modules/content/init.js
@@ -0,0 +1,17 @@
+import handlebars from 'handlebars';
+
+//template
+import contentTemplate from './contentTemplate.html!text';
+
+//data
+import contentData from './contentData.json!';
+
+export function createContent() {
+
+  var template = Handlebars.compile(contentTemplate);
+  var html    = template(contentData);
+
+  $('.container').append(html);
+
+  console.timeEnd('Module');
+}
