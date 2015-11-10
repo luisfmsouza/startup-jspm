4c5c00798d8632894cb54c9d4f9a82065378d9f9
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-11-10 11:23:59 -0200

    Fixing livereload issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect');
- 
+
 gulp.task('connect', function() {
   connect.server({
     root: './',
     livereload: true
   });
 });
- 
+
 gulp.task('html', function () {
   gulp.src('./*.html')
     .pipe(connect.reload());
 });
- 
+
 gulp.task('js', function () {
-  connect.reload();
+  gulp.src('./*.js')
+    .pipe(connect.reload());
 });
- 
+
 gulp.task('watch', function () {
   gulp.watch(['./*.html'], ['html']);
-  gulp.watch(['./js/*.js', './*.js'], ['js']);
+  gulp.watch(['./modules/**/*.js', './*.js'], ['js']);
 });
- 
+
 gulp.task('default', ['connect', 'watch']);
