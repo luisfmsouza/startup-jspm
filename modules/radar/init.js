3dab4ff1053251efff35b2f411b64ec6eec3cc46
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-11-16 16:45:48 -0200

    Load radar module

--- modules/radar/init.js
+++ modules/radar/init.js
@@ -11,7 +11,7 @@ export function createContent() {
   var template = Handlebars.compile(contentTemplate);
   var html    = template(contentData);
 
-  $('#content').append(html);
+  $('#content').html(html);
 
   console.timeEnd('Module Radar');
 }
