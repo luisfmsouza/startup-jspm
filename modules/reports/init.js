53ffb5991aa3c85dd198cafc06487dc98d77dcb2
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-11-17 17:29:20 -0200

    adding other behavior to handle translations

--- modules/reports/init.js
+++ modules/reports/init.js
@@ -5,27 +5,27 @@ import handlebars from 'handlebars';
 //template
 import contentTemplate from './contentTemplate.html!text';
 
+// Handlebars helper to translate
+Handlebars.registerHelper('t', function(i18n_key) {
+  var result = i18n.t(i18n_key);
+
+  return new Handlebars.SafeString(result);
+});
+
 //data
 import contentData from './contentData.json!';
 
 export function createContent() {
 
+  i18n.loadNamespace('../../modules/reports/locales/en-US/reports', function() {
+    console.log(i18n.t("../../modules/reports/locales/en-US/reports:page.name"));
+    console.log(i18n);
+  });
+
   var template = Handlebars.compile(contentTemplate);
   var html    = template(contentData);
 
   $('#content').html(html);
-
-  // i18n.loadNamespace('reports', function() {
-  //   console.log(i18n.t("reports:app.name"));
-  //   console.log(i18n.t("reports:report"));
-  // });
-
-
-  var options = { resGetPath: 'modules/' + moduleName + '/locales/__lng__/__ns__.json' };
-
-  i18n.init(options, function () {
-    console.log(i18n.t("app.name"));
-  });
 
   console.timeEnd('Module reports');
 }
